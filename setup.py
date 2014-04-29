0e208ae7ed67d7fa3865752c64398d87b57efbbc
Darius <bakunas@gmail.com>
2014-04-29 18:22:24 -0400

    Modified license field in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from distutils.core import setup
 
 setup(
     name='rawdisk',
+    author='D. Bakunas'
     version='0.1dev',
     description='Experimental python code to learn different disk formats',
     packages=['rawdisk',],
-    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+    license='LICENSE.txt',
     long_description=open('README.txt').read(),
+    install_requires=[
+        "hexdump >= 2.0",
+    ]
 )
