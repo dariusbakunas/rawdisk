01102f75a5b89f325ddcf3da2a91c1a98a9d5681
Darius <bakunas@gmail.com>
2014-04-29 17:10:29 -0400

    Created initial setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='rawdisk',
+    version='0.1dev',
+    description='Experimental python code to learn different disk formats',
+    packages=['rawdisk',],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+    long_description=open('README.txt').read(),
+)
