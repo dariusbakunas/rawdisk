246234bec50d80a8bd09dcb089b67d2f65f66e0e
Darius <bakunas@gmail.com>
2014-04-29 22:18:47 -0400

    Added argument parsing to rawdisk command

--- rawdisk/main.py
+++ rawdisk/main.py
@@ -1,5 +1,23 @@
+from optparse import OptionParser
+import os
+import sys
+
+
 def main():
-    pass
+    parser = OptionParser(
+        usage='Usage: %s -f <source>' %
+        os.path.basename(sys.argv[0])
+    )
+
+    parser.add_option(
+        '-f', '--file', dest='filename', type='string',
+        help='specify source file'
+    )
+
+    (options, args) = parser.parse_args()
+
+    if options.filename is None:
+        parser.error('Filename not given')
 
 if __name__ == "__main__":
     main()
