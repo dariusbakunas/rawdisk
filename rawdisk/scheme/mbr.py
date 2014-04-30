de62a69ea362d9bd4b98fb138650c3de780f14a6
Darius <bakunas@gmail.com>
2014-04-30 12:54:39 -0400

    Added Volume Boot Record (VBR) class

--- rawdisk/scheme/mbr.py
+++ rawdisk/scheme/mbr.py
@@ -10,8 +10,21 @@ PT_TABLE_SIZE = PT_ENTRY_SIZE * 4
 SECTOR_SIZE = 512
 
 
-class PartitionEntry:
+class VBR:
     def load(self, raw_data):
+        self.raw = raw_data
+        self.oem_id = struct.unpack("<8s", raw_data[3:11])[0]
+        
+    def hexdump(self):
+        hexdump.hexdump(self.raw)
+
+
+class PartitionEntry:
+    def __init__(self):
+        self.vbr = VBR()
+        
+    def load(self, raw_data):
+        self.raw = raw_data
         self.boot_indicator = struct.unpack("<B", raw_data[:1])[0]
         self.starting_head = struct.unpack("<B", raw_data[1:2])[0]
         tmp = struct.unpack("<B", raw_data[2:3])[0]
@@ -34,17 +47,16 @@ class PartitionTable:
         self.entries = []
 
     def load(self, raw_data):
-        self.raw = raw_data[
-            PT_TABLE_OFFSET:PT_TABLE_OFFSET + PT_TABLE_SIZE
-        ]
+        self.raw = raw_data
 
         for i in range(0, 4):
             start = PT_ENTRY_SIZE * i
             end = start + PT_ENTRY_SIZE
             entry = PartitionEntry()
             entry.load(self.raw[start:end])
+            
             if (entry.part_type != 0):
-                    self.entries.append(entry)
+                self.entries.append(entry)
 
     def hexdump(self):
         hexdump.hexdump(self.raw)
@@ -54,16 +66,27 @@ class MBR:
     def __init__(self):
         self.partition_table = PartitionTable()
 
-    def load(self, raw_data):
-        signature = struct.unpack("<H", raw_data[
+    def load(self, source):
+        source.seek(0)
+        mbr_data = source.read(512)
+        
+        signature = struct.unpack("<H", mbr_data[
             MBR_SIG_OFFSET:MBR_SIG_OFFSET+MBR_SIG_SIZE
         ])[0]
 
         if (signature != MBR_SIGNATURE):
             return False
 
-        self.raw = raw_data
-        self.partition_table.load(self.raw)
+        self.raw = mbr_data
+        self.partition_table.load(self.raw[
+            PT_TABLE_OFFSET:PT_TABLE_OFFSET + PT_TABLE_SIZE
+        ])            
+        
+        for entry in self.partition_table.entries:
+            source.seek(entry.part_offset)
+            vbr_data = source.read(SECTOR_SIZE)            
+            entry.vbr.load(vbr_data)
+            entry.vbr.hexdump()
 
         return True
 
