bee3577c1c9170c93e0103e40d469e8a55a2f4b2
Darius <bakunas@gmail.com>
2014-05-01 10:38:17 -0400

    Modified detect_scheme method
    Accept filename instead of source

--- rawdisk/scheme/common.py
+++ rawdisk/scheme/common.py
@@ -2,33 +2,38 @@ import mbr
 import gpt
 import struct
 import hexdump
+import sys
 
 SCHEME_UNKNOWN = 0x1
 SCHEME_MBR = 0x2
 SCHEME_GPT = 0x4
 
 
-def detect_scheme(source):
+def detect_scheme(filename):
     """Detects partitioning scheme of the source
 
     Returns SCHEME_MBR, SCHEME_GPT or SCHEME_UNKNOWN
     """
 
-    # Look for MBR signature first
-    source.seek(mbr.MBR_SIG_OFFSET)
-    data = source.read(mbr.MBR_SIG_SIZE)
-    signature = struct.unpack("<H", data)[0]
+    try:
+        with open(filename, 'rb') as f:
+            # Look for MBR signature first
+            f.seek(mbr.MBR_SIG_OFFSET)
+            data = f.read(mbr.MBR_SIG_SIZE)
+            signature = struct.unpack("<H", data)[0]
 
-    if (signature != mbr.MBR_SIGNATURE):
-        # Something else
-        return SCHEME_UNKNOWN
-    else:
-        # Could be MBR or GPT, look for GPT header
-        source.seek(gpt.GPT_HEADER_OFFSET)
-        data = source.read(gpt.GPT_SIG_SIZE)
-        signature = struct.unpack("<8s", data)[0]
+            if (signature != mbr.MBR_SIGNATURE):
+                # Something else
+                return SCHEME_UNKNOWN
+            else:
+                # Could be MBR or GPT, look for GPT header
+                f.seek(gpt.GPT_HEADER_OFFSET)
+                data = f.read(gpt.GPT_SIG_SIZE)
+                signature = struct.unpack("<8s", data)[0]
 
-        if (signature != gpt.GPT_SIGNATURE):
-            return SCHEME_MBR
-        else:
-            return SCHEME_GPT
+                if (signature != gpt.GPT_SIGNATURE):
+                    return SCHEME_MBR
+                else:
+                    return SCHEME_GPT
+    except IOError, e:
+        print e    
