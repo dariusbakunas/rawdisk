7df504f7630636e2d6d724796db820e57410063b
Darius <bakunas@gmail.com>
2014-05-01 17:38:26 -0400

    Renamed couple things

--- rawdisk/filesystems/common.py
+++ rawdisk/filesystems/common.py
@@ -2,9 +2,9 @@ from rawdisk.util.rawstruct import RawStruct
 import hexdump
 
 
-PART_TYPE_UNKNOWN = 0x00
-PART_TYPE_NTFS = 0x01
-PART_TYPE_EXFAT = 0x02
+PART_FORMAT_UNKNOWN = 0x00
+PART_FORMAT_NTFS = 0x01
+PART_FORMAT_EXFAT = 0x02
 
 
 def is_type_ntfs(data):
@@ -12,27 +12,37 @@ def is_type_ntfs(data):
     oem_id = pt.get_string(3, 8)
 
     if (oem_id == "NTFS    "):
-        return PART_TYPE_NTFS
+        return PART_FORMAT_NTFS
     else:
-        return PART_TYPE_UNKNOWN
+        return PART_FORMAT_UNKNOWN
 
 
 def is_type_exfat(data):
-    return PART_TYPE_UNKNOWN
+    return PART_FORMAT_UNKNOWN
 
 type_callbacks = {
     0x7: [is_type_ntfs, is_type_exfat],
 }
 
 
-def detect_partition_type(data, type_id):
-    for callback in type_callbacks[type_id]:
-        result = callback(data)
-        if result != PART_TYPE_UNKNOWN:
-            return result
+def detect_partition_format(filename, offset, type_id):
+    data = None
 
-    return PART_TYPE_UNKNOWN
+    try:
+        with open(filename, 'rb') as f:
+            f.seek(offset)
+            data = f.read(512)  # not sure yet if 512 is enough
+
+            for callback in type_callbacks[type_id]:
+                result = callback(data)
+                if result != PART_FORMAT_UNKNOWN:
+                    return result
+    except IOError, e:
+        print e
+
+    return PART_FORMAT_UNKNOWN
 
 
 class Partition:
-    pass
+    def __init__(self):
+        pass
