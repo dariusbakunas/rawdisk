6020061f52e0f1b22e82fb557fcd84abbd97918e
Darius <bakunas@gmail.com>
2014-05-02 12:11:25 -0400

    Added __str__ to MFT_Entry

--- rawdisk/filesystems/ntfs.py
+++ rawdisk/filesystems/ntfs.py
@@ -26,31 +26,72 @@ class MFT_Entry_Header(RawStruct):
     def load(self, data):
         RawStruct.data.fset(self, data)
         self.file_signature = self.get_string(0, 4)
-        
+        self.update_seq_array_offset = self.get_ushort(4)
+        self.update_seq_array_size = self.get_ushort(6)
+        self.logfile_seq_number = self.get_ulonglong(8)
+        self.seq_number = self.get_ushort(16)
+        self.hard_link_count = self.get_ushort(18)
+        self.first_attr_offset = self.get_ushort(20)
+        self.flags = self.get_ushort(22)
+        self.used_size = self.get_uint(24)
+        self.allocated_size = self.get_ushort(28)
+        self.base_file_record = self.get_ulonglong(30)
+        self.next_attr_id = self.get_ushort(38)
+        self.mft_record_number = self.get_uint(42)
+
+
+class MFT_Attribute_Header(RawStruct):
+    def __init__(self):
+        pass
+
+class MFT_Attribute(RawStruct):
+    def __init__(self):
+        pass
 
 class MFT_Entry(RawStruct):
     def __init__(self, offset):
         self.offset = offset
         self.header = MFT_Entry_Header()
+        self.attributes = []
 
     def load(self, data):
         RawStruct.data.fset(self, data)
         self.header.load(data)
-        
+
+        # print "Attr offset:", hex(self.header.first_attr_offset)
+
+    @property
+    def used_size(self):
+        return self.header.used_size
+
+    @property
+    def size(self):
+        return self.header.allocated_size
+
+    def __str__(self):
+        return "MFT Record no: %d, " \
+                "Offset: 0x%x, " \
+                "Size: %d, " \
+                "Used Size: %d, " \
+                "Signature: %s" % (
+            self.header.mft_record_number,
+            self.offset,
+            self.size,
+            self.used_size,
+            self.header.file_signature
+        )
 
 class MFT_Table:
-    def __init__(self):
-        self.offset = 0
+    def __init__(self, offset):
+        self.offset = offset
         self.mft_entries = []
 
-    def load(self, source, offset):
-        self.offset = offset
+    def load(self, source):
         source.seek(self.offset)
         data = source.read(MFT_ENTRY_SIZE)
         first_mft_entry = MFT_Entry(self.offset)
         first_mft_entry.load(data)
-        first_mft_entry.hexdump()
-
+        print first_mft_entry
 
 class BPB(RawStruct):
     """Bios parameter block
@@ -93,8 +134,8 @@ class NTFS_Partition(Partition):
                 data = f.read(512)
                 self.bootsector.load(data)                
                 self.root_mft_offset = self.get_root_mft_offset()
-                self.mft_table = MFT_Table()
-                self.mft_table.load(f, self.root_mft_offset)
+                self.mft_table = MFT_Table(self.root_mft_offset)
+                self.mft_table.load(f)
 
 
         except IOError, e:
