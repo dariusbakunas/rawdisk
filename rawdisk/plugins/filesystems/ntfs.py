dda7df344c0ca92075e752d1abfdcf1ddef86fff
Darius <bakunas@gmail.com>
2014-05-25 14:06:37 -0400

    NtfsPlugin now returns correct volume fixes #2

--- rawdisk/plugins/filesystems/ntfs.py
+++ rawdisk/plugins/filesystems/ntfs.py
@@ -1,11 +1,34 @@
+import uuid
 import rawdisk.plugins.categories as categories
+from rawdisk.filesystems.detector import FilesystemDetectorSingleton
+from rawdisk.util.rawstruct import RawStruct
+from rawdisk.filesystems.ntfs import NtfsVolume
+
+SIG_DATA_SIZE = 512
 
 class NtfsPlugin(categories.IFilesystemPlugin):
     def register(self):
-        print "ntfs was registered"
+        detector = FilesystemDetectorSingleton.get()
+        detector.add_mbr_plugin(0x07, self)
+        detector.add_gpt_plugin(
+            uuid.UUID('{EBD0A0A2-B9E5-4433-87C0-68B6B72699C7}'),
+            self
+        )
 
-    def detect_mbr(self, filename, offset, type_id): 
-        print "call: detect_mbr from NtfsPlugin"
+    def detect(self, filename, offset): 
+        try:
+            with open(filename, 'rb') as f:
+                f.seek(offset)
+                data = f.read(SIG_DATA_SIZE)
+                rs = RawStruct(data)
+                oem_id = rs.get_string(0x03, 8)
 
-    def detect_gpt(self, filename, offset, type_guid):
-        print "call: detect_gpt from NtfsPlugin"
+                if (oem_id == "NTFS    "):
+                    return True
+        except IOError, e:
+            print e
+
+        return None
+
+    def get_volume_object(self):
+        return NtfsVolume()
