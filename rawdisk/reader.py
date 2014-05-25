31649f1c25b47bb8fabb283b5c6d9ba659ca0ecf
Darius <bakunas@gmail.com>
2014-05-25 14:05:13 -0400

    Implemented FilesystemDetector, #2
    This module will use filesystem detection plugins that reside in
    plugins/filesystems directory

--- rawdisk/reader.py
+++ rawdisk/reader.py
@@ -3,9 +3,8 @@ import hexdump
 import scheme
 import rawdisk.filesystems
 from rawdisk.filesystems.common import *
-from rawdisk.filesystems.ntfs import NtfsVolume
 from rawdisk.filesystems.hfs_plus import HfsPlusVolume
-
+from rawdisk.filesystems.detector import FilesystemDetectorSingleton
 
 class Reader:
     def __init__(self):
@@ -21,6 +20,7 @@ class Reader:
 
         # Detect partitioning scheme
         self.scheme = scheme.common.detect_scheme(filename)
+        detector = FilesystemDetectorSingleton.get()
 
         if (self.scheme == scheme.common.SCHEME_MBR):
             print "Scheme: MBR"
@@ -29,16 +29,15 @@ class Reader:
 
             # Go through table entries and analyse ones that are supported
             for entry in mbr.partition_table.entries:
-                pt_format = detect_mbr_partition_format(
-                    filename,
-                    entry.part_offset,
+                volume = detector.detect_mbr(
+                    filename, 
+                    entry.part_offset, 
                     entry.part_type
                 )
 
-                if pt_format == rawdisk.filesystems.common.PART_FORMAT_NTFS:
-                    partition = NtfsVolume()
-                    partition.load(filename, entry.part_offset)
-                    self.partitions.append(partition)
+                if (volume!=None):
+                    volume.load(filename, entry.part_offset)
+                    self.partitions.append(volume)
 
         elif (self.scheme == scheme.common.SCHEME_GPT):
             print "Scheme: GPT"
@@ -46,21 +45,15 @@ class Reader:
             gpt.load(filename)
             
             for entry in gpt.partition_entries:
-                pt_format = detect_gpt_partition_format(
+                volume = detector.detect_gpt(
                     filename,
                     entry.first_lba * 512,
-                    entry.type_guid)
+                    entry.type_guid
+                )
 
-                if pt_format == rawdisk.filesystems.common.PART_FORMAT_HFS_PLUS:
-                    partition = HfsPlusVolume()
-
-                    # TODO: Figure out how to calculate block size
-                    partition.load(filename, entry.first_lba * 512)
-                    self.partitions.append(partition)
-                elif pt_format == rawdisk.filesystems.common.PART_FORMAT_NTFS:
-                    partition = NtfsVolume()
-                    partition.load(filename, entry.first_lba * 512)
-                    self.partitions.append(partition)
+                if (volume != None):
+                    volume.load(filename, entry.first_lba * 512)
+                    self.partitions.append(volume)
 
         elif (self.scheme == scheme.common.SCHEME_UNKNOWN):
             print 'Partitioning scheme is not supported.'
