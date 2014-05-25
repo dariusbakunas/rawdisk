b662bc62150fb9c3a4d6ff0115abd9a84dfac096
Darius <bakunas@gmail.com>
2014-05-25 14:31:22 -0400

    Moved ntfs files under ntfs plugin directory

--- rawdisk/plugins/filesystems/ntfs/ntfs.py
+++ rawdisk/plugins/filesystems/ntfs/ntfs.py
@@ -1,14 +1,45 @@
+import uuid
+import hurry.filesize
+import hexdump
 from rawdisk.filesystems.volume import Volume
 from rawdisk.util.rawstruct import RawStruct
-import hurry.filesize
+import rawdisk.plugins.categories as categories
+from rawdisk.filesystems.detector import FilesystemDetectorSingleton
 from mft import *
-import hexdump
 
 BPB_SIZE = 25
 BPB_OFFSET = 0x0B
 EXTENDED_BPB_SIZE = 48
 NTFS_BOOTSECTOR_SIZE = 512
 
+SIG_DATA_SIZE = 512
+
+class NtfsPlugin(categories.IFilesystemPlugin):
+    def register(self):
+        detector = FilesystemDetectorSingleton.get()
+        detector.add_mbr_plugin(0x07, self)
+        detector.add_gpt_plugin(
+            uuid.UUID('{EBD0A0A2-B9E5-4433-87C0-68B6B72699C7}'),
+            self
+        )
+
+    def detect(self, filename, offset): 
+        try:
+            with open(filename, 'rb') as f:
+                f.seek(offset)
+                data = f.read(SIG_DATA_SIZE)
+                rs = RawStruct(data)
+                oem_id = rs.get_string(0x03, 8)
+
+                if (oem_id == "NTFS    "):
+                    return True
+        except IOError, e:
+            print e
+
+        return None
+
+    def get_volume_object(self):
+        return NtfsVolume()
 
 class Bpb(RawStruct):
     """Bios parameter block
