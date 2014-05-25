517742cbf787ad7ac09a518c34307ac3c2e561ba
Darius <bakunas@gmail.com>
2014-05-25 10:07:00 -0400

    Make sure plugin configuration files are included

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='rawdisk',
     author='D. Bakunas',
-    version='0.1dev',
+    version='0.2dev',
     description='Experimental python code to learn different disk formats',
     packages=find_packages(),
+    package_data = {'rawdisk.plugins.filesystems' : ['*.yapsy-plugin']},
     license='LICENSE.txt',
     long_description=open('README.md').read(),
     install_requires=[
