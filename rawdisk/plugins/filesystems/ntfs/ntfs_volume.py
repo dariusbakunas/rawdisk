0b67367a64d628274315f446ce32df372c65b3f0
Darius <bakunas@gmail.com>
2014-05-30 16:48:46 -0400

    Fixed MIT license headers for couple files

--- rawdisk/plugins/filesystems/ntfs/ntfs_volume.py
+++ rawdisk/plugins/filesystems/ntfs/ntfs_volume.py
@@ -2,7 +2,7 @@
 
 # The MIT License (MIT)
 
-# Copyright (c) 2004 Darius Bakunas
+# Copyright (c) 2014 Darius Bakunas
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
