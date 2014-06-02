ca8a83efe501251cec3be6ae62523c07199cad9f
Darius <bakunas@gmail.com>
2014-06-01 20:30:24 -0400

    Helper functions for setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,20 @@
 import os
 import sys
 import imp
+import subprocess
 from setuptools import setup, find_packages
+
+## Python 2.6 subprocess.check_output compatibility. Thanks Greg Hewgill!
+if 'check_output' not in dir(subprocess):
+    def check_output(cmd_args, *args, **kwargs):
+        proc = subprocess.Popen(
+            cmd_args, *args,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
+        out, err = proc.communicate()
+        if proc.returncode != 0:
+            raise subprocess.CalledProcessError(args)
+        return out
+    subprocess.check_output = check_output
 
 # Add the current directory to the module search path.
 sys.path.append('.')
@@ -26,6 +39,61 @@ TESTS_DIRECTORY = 'tests'
 # the setup_requires keyword.
 metadata = imp.load_source(
     'metadata', os.path.join(CODE_DIRECTORY, 'metadata.py'))
+
+# Helper functions
+def git_ls_files(*cmd_args):
+    """Run ``git ls-files`` in the top-level project directory. Arguments go
+    directly to execution call.
+
+    Returns:
+        set of file names
+    """
+    cmd = ['git', 'ls-files']
+    cmd.extend(cmd_args)
+    return set(subprocess.check_output(cmd).splitlines())
+
+def get_git_project_files():
+    """Retrieve a list of all non-ignored files, including untracked files,
+    excluding deleted files.
+
+    Returns:
+        sorted list of git project files
+    """
+    cached_and_untracked_files = git_ls_files(
+        '--cached',  # All files cached in the index
+        '--others',  # Untracked files
+        # Exclude untracked files that would be excluded by .gitignore, etc.
+        '--exclude-standard')
+    uncommitted_deleted_files = git_ls_files('--deleted')
+
+    # Since sorting of files in a set is arbitrary, return a sorted list to
+    # provide a well-defined order to tools like flake8, etc.
+    return sorted(cached_and_untracked_files - uncommitted_deleted_files)
+
+def is_git_project():
+    return os.path.isdir('.git')
+
+def get_project_files():
+    """Retrieve a list of project files, ignoring hidden files.
+
+    Returns:
+        sorted list of project files
+    """
+    if is_git_project():
+        return get_git_project_files()
+
+    project_files = []
+    for top, subdirs, files in os.walk('.'):
+        for subdir in subdirs:
+            if subdir.startswith('.'):
+                subdirs.remove(subdir)
+
+        for f in files:
+            if f.startswith('.'):
+                continue
+            project_files.append(os.path.join(top, f))
+
+    return project_files
 
 def read(filename):
     """Return the contents of a file.
