204f88adb9213dbff5bb26d5506470588e50cfe7
Darius <bakunas@gmail.com>
2014-06-02 22:30:41 -0400

    Introduced first unit tests

---
+++ tests/test_utils.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('../'))
+
+import unittest
+from rawdisk.util.filetimes import dt_to_filetime, filetime_to_dt
+from datetime import datetime
+
+
+class TestFiletimesModule(unittest.TestCase):
+    def test_dt_to_filetime(self):
+        value = datetime(2009, 7, 25, 23, 0)
+        self.assertEquals(128930364000000000, dt_to_filetime(value))
+
+    def test_filetime_to_dt(self):
+        value = 116444736000000000
+        self.assertEquals(datetime(1970, 1, 1, 0, 0), filetime_to_dt(value))
+
+if __name__ == "__main__":
+    unittest.main()
