ca0c243334fd377b6e93687b79ec381d53d93cb2
Darius <bakunas@gmail.com>
2014-06-13 15:07:33 -0400

    Renamed mft attr header property

--- rawdisk/plugins/filesystems/ntfs/mft_attr_header.py
+++ rawdisk/plugins/filesystems/ntfs/mft_attr_header.py
@@ -53,7 +53,7 @@ class MftAttrHeader(RawStruct):
         covered by this attribute record.
         highest_vcn (ulonglong): The highest VCN covered by this \
         attribute record.
-        mapping_pairs_offset (ushort): The offset to the mapping \
+        data_run_offset (ushort): The offset to the mapping \
         pairs array from the start of the attribute record, in bytes.
         comp_unit_size (ushort): Compression unit size = 2 x clusters. \
         0 implies uncompressed.
@@ -89,7 +89,7 @@ class MftAttrHeader(RawStruct):
             # Attribute is Non-Resident
             self.lowest_vcn = self.get_ulonglong_le(0x10)
             self.highest_vcn = self.get_ulonglong_le(0x18)
-            self.mapping_pairs_offset = self.get_ushort_le(0x20)
+            self.data_run_offset = self.get_ushort_le(0x20)
             self.comp_unit_size = self.get_ushort_le(0x22)
             # 4 byte 0x00 padding @ 0x24
             self.alloc_size = self.get_ulonglong_le(0x28)
@@ -99,7 +99,9 @@ class MftAttrHeader(RawStruct):
             if (self.length_of_name > 0):
                 self.attr_name = self.get_chunk(
                     0x40, 2 * self.length_of_name).decode('utf-16')
-                # print self.attr_name.decode('utf-16')
+            
+            # data runs start @ self.data_run_offset
+
         else:
             # Attribute is Resident
             self.attr_length = self.get_uint_le(0x10)
