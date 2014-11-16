c1aa8ce52d91f19e120f4234775cfd88e6375d47
Darius Bakunas-Milanowski <bakunas@gmail.com>
2014-11-15 19:23:34 -0500

    Remove license headers from source files

--- rawdisk/util/filetimes.py
+++ rawdisk/util/filetimes.py
@@ -24,6 +24,7 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 """Tools to convert between Python datetime instances and Microsoft times.
 
 Source: http://reliablybroken.com\
