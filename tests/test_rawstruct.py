643f14f28460bd2267bb771c1ad96f8f0cceda42
Darius Bakunas-Milanowski <bakunas@gmail.com>
2014-11-17 22:05:39 -0500

    Refactored rawstruct test

--- tests/test_rawstruct.py
+++ tests/test_rawstruct.py
@@ -13,11 +13,12 @@ class TestRawStruct(unittest.TestCase):
     def setUp(self):
         self.sample_data = b'\xa1\xb1\xc1\xd1\xe1\xf1\xb1\xa1\xc1'
         self.sample_uuid_data = b'\x12\x34\x56\x78'*4
+        self.rwstruct = RawStruct(self.sample_data)
 
     def test_init_from_data(self):
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            (r.size, r.data), (len(self.sample_data), self.sample_data))
+            (self.rwstruct.size, self.rwstruct.data),
+            (len(self.sample_data), self.sample_data))
 
     def test_init_from_data_with_offset(self):
         offset = 2
@@ -57,79 +58,68 @@ class TestRawStruct(unittest.TestCase):
         offset = 2
         format = "B"
         length = 1
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_field(offset, length, format),
+            self.rwstruct.get_field(offset, length, format),
             struct.unpack(format, self.sample_data[offset:offset+length])[0])
 
     def test_get_uchar(self):
         offset = 2
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_uchar(offset),
+            self.rwstruct.get_uchar(offset),
             struct.unpack("B", self.sample_data[offset:offset+1])[0])
 
     def test_get_ushort_le(self):
         offset = 0
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_ushort_le(offset),
+            self.rwstruct.get_ushort_le(offset),
             struct.unpack("<H", self.sample_data[offset:offset+2])[0])
 
     def test_get_ushort_be(self):
         offset = 2
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_ushort_be(offset),
+            self.rwstruct.get_ushort_be(offset),
             struct.unpack(">H", self.sample_data[offset:offset+2])[0])
 
     def test_get_uint_le(self):
         offset = 0
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_uint_le(offset),
+            self.rwstruct.get_uint_le(offset),
             struct.unpack("<I", self.sample_data[offset:offset+4])[0])
 
     def test_get_uint_be(self):
         offset = 0
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_uint_be(offset),
+            self.rwstruct.get_uint_be(offset),
             struct.unpack(">I", self.sample_data[offset:offset+4])[0])
 
     def test_get_int_le(self):
         offset = 0
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_int_le(offset),
+            self.rwstruct.get_int_le(offset),
             struct.unpack("<I", self.sample_data[offset:offset+4])[0])
 
     def test_get_ulong_le(self):
         offset = 0
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_ulong_le(offset),
+            self.rwstruct.get_ulong_le(offset),
             struct.unpack("<L", self.sample_data[offset:offset+4])[0])
 
     def test_get_ulong_be(self):
         offset = 0
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_ulong_be(offset),
+            self.rwstruct.get_ulong_be(offset),
             struct.unpack(">L", self.sample_data[offset:offset+4])[0])
 
     def test_get_ulonglong_le(self):
         offset = 0
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_ulonglong_le(offset),
+            self.rwstruct.get_ulonglong_le(offset),
             struct.unpack("<Q", self.sample_data[offset:offset+8])[0])
 
     def test_get_ulonglong_be(self):
         offset = 0
-        r = RawStruct(data=self.sample_data)
         self.assertEqual(
-            r.get_ulonglong_be(offset),
+            self.rwstruct.get_ulonglong_be(offset),
             struct.unpack(">Q", self.sample_data[offset:offset+8])[0])
 
     def test_get_uuid_le(self):
@@ -146,7 +136,6 @@ class TestRawStruct(unittest.TestCase):
 
     def test_export(self):
         test_stream = io.BytesIO()
-        r = RawStruct(data=self.sample_data)
-        r._export_to_fd(test_stream)
+        self.rwstruct._export_to_fd(test_stream)
         test_stream.seek(0)
         self.assertEqual(self.sample_data, test_stream.readline())
