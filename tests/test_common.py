7d31e925f5fb8cee76903c9c9ba1c4276163677d
Darius Bakunas-Milanowski <bakunas@gmail.com>
2014-11-21 19:31:22 -0500

    Added tests for scheme detection #26

---
+++ tests/test_common.py
@@ -0,0 +1,17 @@
+import unittest
+from rawdisk.scheme.common import detect_scheme, \
+    SCHEME_MBR, SCHEME_GPT, SCHEME_UNKNOWN
+
+
+class TestCommon(unittest.TestCase):
+    def test_detect_mbr_scheme(self):
+        scheme = detect_scheme('sample_images/ntfs_mbr.vhd')
+        self.assertEquals(SCHEME_MBR, scheme)
+
+    def test_detect_gpt_scheme(self):
+        scheme = detect_scheme('sample_images/ntfs_primary_gpt.bin')
+        self.assertEquals(SCHEME_GPT, scheme)
+
+    def test_detect_unknown_scheme(self):
+        scheme = detect_scheme('sample_images/ntfs_mft_table.bin')
+        self.assertEquals(SCHEME_UNKNOWN, scheme)
