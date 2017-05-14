cefdc59203555503c14ad080b4b1fa236ab65f38
darius.bakunas <dbakuna@us.ibm.com>
2017-05-14 09:04:50 -0400

    add logging setup and switch to argparse (py3)

--- rawdisk/main.py
+++ rawdisk/main.py
@@ -1,40 +1,70 @@
 # -*- coding: utf-8 -*-
 
-from optparse import OptionParser
+import argparse
 import os
 import sys
 import rawdisk
-import logging
+import logging.config
+import yaml
 from . import scheme
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
+def setup_logging(config_path='logging.yaml', default_level=logging.INFO):
+    """Setup logging configuration
+    """
+    path = config_path
 
-    parser = OptionParser(
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+
+def main():
+    parser = argparse.ArgumentParser(
         usage='Usage: %s -f <source>' %
         os.path.basename(sys.argv[0])
     )
 
-    parser.add_option(
-        '-f', '--file', dest='filename', type='string',
-        help='specify source file'
+    parser.add_argument(
+        "--verbose", help="increase output verbosity", action="store_true")
+
+    parser.add_argument(
+        '-f', '--file', dest='filename', help='specify source file',
     )
 
-    (options, args) = parser.parse_args()
+    parser.add_argument(
+        '--log-config', dest='log_config', help='path to logging configuration file'
+    )
 
-    if options.filename is None:
-        parser.error('Filename not given')
+    parser.add_argument(
+        '--log-level', dest='log_level', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+    )
 
-    r = rawdisk.reader.Reader()
-    r.load(options.filename)
+    args = parser.parse_args()
 
-    if (r.scheme == scheme.common.SCHEME_MBR):
-        print("Scheme: MBR")
-    elif (r.scheme == scheme.common.SCHEME_GPT):
-        print("Scheme: GPT")
+    setup_logging()
+    logger = logging.getLogger(__name__)
+
+    if args.filename is None:
+        parser.print_help()
     else:
-        print("Scheme: Unknown")
+        r = rawdisk.reader.Reader()
 
-    print("Partitions:")
-    r.list_partitions()
+        try:
+            r.load(args.filename)
+        except IOError:
+            logger.error('Failed to open disk image file: {}'.format(args.filename))
+
+        if r.scheme == scheme.common.SCHEME_MBR:
+            print("Scheme: MBR")
+        elif r.scheme == scheme.common.SCHEME_GPT:
+            print("Scheme: GPT")
+        else:
+            print("Scheme: Unknown")
+
+        print("Partitions:")
+        r.list_partitions()
