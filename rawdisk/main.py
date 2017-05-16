d2c75cfead02f1f10ed62b0502fb77f7defbcfb9
Darius Bakunas-Milanowski <bakunas@gmail.com>
2017-05-16 08:08:44 -0400

    minor

--- rawdisk/main.py
+++ rawdisk/main.py
@@ -15,7 +15,7 @@ def setup_logging(config_path=None, log_level=logging.INFO):
 
     config = {
         'version': 1,
-        'disable_existing_loggers': False,  # this fixes the problem
+        'disable_existing_loggers': False,
         'formatters': {
             'standard': {
                 'format':
