aa1eb4b88d9d1a1c11b6e99704c31b9ff1e7b996
Darius Bakunas-Milanowski <bakunas@gmail.com>
2017-05-16 21:18:46 -0400

    refactor: make things private that supposed to be private

--- rawdisk/plugins/manager.py
+++ rawdisk/plugins/manager.py
@@ -21,30 +21,35 @@ class Manager(object):
 
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.fs_plugins = []
-        self.search_path = []
+        self.__fs_plugins = []
 
     def load_plugins(self):
         self.logger.info('Loading filesystem plugins')
-        self._initialize_search_path()
-        self._load_filesystem_plugins()
-        self._register_filesystem_plugins()
+        search_path = self.__get_search_path()
+        self.__load_filesystem_plugins(search_path=search_path)
+        self.__register_filesystem_plugins()
 
-    def _initialize_search_path(self):
-        self.search_path = [os.path.join(
+    @property
+    def filesystem_plugins(self):
+        return self.__fs_plugins
+
+    def __get_search_path(self):
+        search_path = [os.path.join(
             os.path.dirname(rawdisk.__file__), "plugins/filesystems"), ]
 
-        self.search_path += [os.path.join(
+        search_path += [os.path.join(
             path, APP_NAME, "plugins/filesystems") for path in xdg_data_dirs]
 
-    def _register_filesystem_plugins(self):
-        for pluginInfo in self.fs_plugins:
+        return search_path
+
+    def __register_filesystem_plugins(self):
+        for pluginInfo in self.__fs_plugins:
             self.logger.debug(
                 'Registering {} filesystem plugin'.format(pluginInfo.name))
 
             pluginInfo.plugin_object.register()
 
-    def _load_filesystem_plugins(self):
+    def __load_filesystem_plugins(self, search_path):
         """Looks for *.yapsy-plugin files and loads them. It calls 'register' \
         method for each plugin, which in turn registers with \
         :class:`FilesystemDetector \
@@ -62,14 +67,14 @@ class Manager(object):
         ])
 
         # Load the plugins from the plugin directory.
-        self.manager = PluginManagerSingleton.get()
-        self.manager.setPluginPlaces(self.search_path)
-        self.manager.setCategoriesFilter({
+        plugin_manager = PluginManagerSingleton.get()
+        plugin_manager.setPluginPlaces(search_path)
+        plugin_manager.setCategoriesFilter({
             "Filesystem": IFilesystemPlugin,
         })
 
         # Load plugins
-        self.manager.collectPlugins()
+        plugin_manager.collectPlugins()
 
-        for pluginInfo in self.manager.getPluginsOfCategory("Filesystem"):
-            self.fs_plugins.append(pluginInfo)
+        for pluginInfo in plugin_manager.getPluginsOfCategory("Filesystem"):
+            self.__fs_plugins.append(pluginInfo)
