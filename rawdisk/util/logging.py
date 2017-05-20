6a42d97f3fff02d4a17a1eb4453961a4bb94c835
Darius Bakunas-Milanowski <bakunas@gmail.com>
2017-05-20 17:51:10 -0400

    move logging into it's own module

---
+++ rawdisk/util/logging.py
@@ -0,0 +1,47 @@
+import logging.config
+import yaml
+import os
+
+def setup_logging(config_path=None, log_level=logging.INFO):
+    """Setup logging configuration
+    """
+
+    config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format':
+                    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            },
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'level': log_level,
+                'formatter': 'standard',
+                'stream': 'ext://sys.stdout'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['console'],
+                'level': log_level,
+                'propagate': True
+            },
+            'yapsy': {
+                'handlers': ['console'],
+                'level': logging.INFO
+            }
+        }
+    }
+
+    if config_path:
+        if os.path.exists(config_path):
+            with open(config_path, 'rt') as f:
+                config = yaml.safe_load(f.read())
+        else:
+            print('Specified path does not exist: {}, '
+                  'using default config'.format(config_path))
+
+    logging.config.dictConfig(config)
