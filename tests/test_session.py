b36b4a8aa1a4fb6fbae84dc38ee300137b3db31e
Darius Bakunas-Milanowski <bakunas@gmail.com>
2017-05-21 08:36:08 -0400

    fix last merge

--- tests/test_session.py
+++ tests/test_session.py
@@ -2,6 +2,8 @@ import unittest
 from rawdisk.session import Session
 from rawdisk.scheme.common import PartitionScheme
 from rawdisk.filesystems.unknown_volume import UnknownVolume
+from rawdisk.scheme.common import PartitionScheme
+
 
 
 class TestSession(unittest.TestCase):
@@ -16,5 +18,11 @@ class TestSession(unittest.TestCase):
         self.assertEqual(self.session.partition_scheme, PartitionScheme.SCHEME_MBR)
         self.assertEqual(len(self.session.volumes), 1)
         self.assertEqual(type(self.session.volumes[0]), UnknownVolume)
+
+    def test_load_mbr(self):
+        self.session.load_plugins()
+        self.session.load(filename='sample_images/ntfs_mbr.vhd')
+        self.assertEqual(self.session.partition_scheme, PartitionScheme.SCHEME_MBR)
+        self.assertEqual(len(self.session.volumes), 1)
 
 
